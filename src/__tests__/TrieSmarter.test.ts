697e681cceab937a087aa5ee5588483ec1b2ea73
Peter Balogh <palexanderbalogh@yahoo.com>
2020-04-15 13:35:17 -0400

    Initial commit

--- src/__tests__/TrieSmarter.test.ts
+++ src/__tests__/TrieSmarter.test.ts
@@ -2,7 +2,7 @@ import { TrieSmarter } from '../'
 
 describe('TrieSmarter', () => {
     it('should work', () => {
-        let varrr = 6
-        expect(varrr > 5).toBe(true)
+        let ts = new TrieSmarter('foo')
+        expect(ts.elements).not.toEqual(['f', 'o', 'o'])
     })
 })
